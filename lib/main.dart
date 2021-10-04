141e3f4f09ea620469edf0173b06eda7102dcccf
Michal Valkoun <valkounmichal@gmail.com>
2021-10-04 14:48:17 +0200

    Main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'my_app_bar.dart';
-import 'devices.dart';
-import 'my_drawer.dart';
-import 'my_carousel.dart';
-import 'options_item.dart';
-import 'format.dart';
-import 'bottom_button.dart';
+import 'device.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,25 +8,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const buttons = ['Night Mode', 'Hourglass', 'Show Date'];
     return MaterialApp(
+      title: "NixieApp",
+      theme: new ThemeData(scaffoldBackgroundColor: const Color(0xFFF5F5F5)),
       home: Scaffold(
-        appBar: MyAppBar(
-          icon: Icons.bluetooth,
-          title: 'Nixie',
-          widget: const Devices(),
-          isNavigator: true,
-        ),
-        drawer: const MyDrawer(),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              MyCarousel(),
-              ...buttons.map((name) => OptionsItem(name: name)),
-              const Format(),
-              const BottomButton(),
-            ]),
-      ),
+          body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerRight,
+            margin: EdgeInsets.only(right: 20, top: 40),
+            child: Image.asset(
+              "assets/logo_horizontal.png",
+              height: 60,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(right: 15),
+                  child: Text(
+                    "ADD NEW DEVICES",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
+                  ),
+                  child: Icon(
+                    Icons.keyboard_arrow_right_outlined,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Device(),
+        ],
+      )),
     );
   }
 }
