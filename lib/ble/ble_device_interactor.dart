9e1425b4a2375029e6ae7f86e58213db0f7f92ab
Michal Valkoun <valkounmichal@gmail.com>
2022-03-29 12:04:08 +0200

    Update jdk and new app version

--- lib/ble/ble_device_interactor.dart
+++ lib/ble/ble_device_interactor.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:noyce_joyce_nixie/constant.dart';
 
