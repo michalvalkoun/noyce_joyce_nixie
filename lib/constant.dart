7f0af180678ecf087bd6ec93bb1372913675d9bc
Michal Valkoun <valkounmichal@gmail.com>
2022-04-08 15:31:12 +0200

    New firmware 1.17. Fixes #37.

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,4 @@
-const latestFwVer = "1.16";
+const latestFwVer = "1.17";
 
 const String infoServiceUuid = "0000180A-0000-1000-8000-00805F9B34FB";
 const String interfaceServiceUuid = "A8ED1400-130A-4D4B-ACBA-5DE7E77E9B47";
